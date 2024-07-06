7636adb441650cab325dd13a6c8ce92a22aed295
Avery Sykes <averysykes69@gmail.com>
2024-07-05 20:10:44 -0500

    add logic to have computer output a value of rock paper or scissors

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 
-console.log("hello world")
+
+
+
+
+
+function getComputerChoice (randomNumber) {
+    randomNumber = Math.floor(Math.random() * 3);
+    return randomNumber;
+}
+
+
+let choice = getComputerChoice()
+
+console.log(choice);
+
+if (choice == 0) {
+    choice = "rock"
+    console.log(choice)
+}
+else if (choice == 1) {
+    choice = "paper"
+    console.log(choice)
+}
+else {
+    choice = "scissors"
+    console.log(choice)
+}
